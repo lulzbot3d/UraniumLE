a4648db8619fe524878a61599fbb80a2856133c5
Victor Larchenko <victor99992@gmail.com>
2016-09-21 13:00:27 +0600

    T496: Fixed some bugs

--- UM/Settings/DefinitionContainer.py
+++ UM/Settings/DefinitionContainer.py
@@ -148,10 +148,15 @@ class DefinitionContainer(ContainerInterface.ContainerInterface, PluginObject):
             value = getattr(definition, property_name)
             if value is None and property_name == "value":
                 value = getattr(definition, "default_value")
-                if (key == "machine_start_gcode" or key == "machine_end_gcode") and getattr(definition, "default_file") != 0:
-                    path = Resources.getPath(Resources.GCodes, getattr(definition, "default_file") + ".gcode")
-                    with open(path, encoding = "utf-8") as f:
-                        return f.read()
+                try:
+                    if (key == "machine_start_gcode" or key == "machine_end_gcode") and getattr(definition, "default_value_from_file") != 0:
+                        path = Resources.getPath(Resources.GCodes, getattr(definition, "default_value_from_file") + ".gcode")
+                        with open(path, encoding = "utf-8") as f:
+                            return f.read()
+                except AttributeError:
+                    return None
+                except FileNotFoundError:
+                    return None
             return value
         except AttributeError:
             return None
